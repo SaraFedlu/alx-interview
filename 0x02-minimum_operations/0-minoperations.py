73a8cd47efd2a09ad0e56f789d76467596895913
Sara Fedlu <sfedlu28@gmail.com>
2024-08-25 10:30:33 +0000

    debug some fails

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,21 +1,22 @@
 #!/usr/bin/python3
-""" Minimum Operations
-    """
-
+"""
+Minimum Operations
+"""
 
 def minOperations(n: int) -> int:
-    """ Minimum Operations needed to get n H characters """
-    next = 'H'
-    body = 'H'
-    op = 0
-    while (len(body) < n):
-        if n % len(body) == 0:
-            op += 2
-            next = body
-            body += body
-        else:
-            op += 1
-            body += next
-    if len(body) != n:
+    """Calculates the fewest number of operations needed to achieve n 'H' characters."""
+    if n <= 1:
         return 0
-    return op
+    
+    operations = 0
+    divisor = 2
+
+    # Factorizing n by dividing by the smallest factors
+    while n > 1:
+        # If the current divisor is a factor, reduce n and add the divisor to the operations count
+        while n % divisor == 0:
+            operations += divisor
+            n //= divisor
+        divisor += 1
+    
+    return operations
