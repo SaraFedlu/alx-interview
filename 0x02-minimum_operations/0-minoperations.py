b4b08e671b45e190e4b0e2fab1bfe7dd29c0fef4
Sara Fedlu <sfedlu28@gmail.com>
2024-08-25 10:35:57 +0000

    fix pep8 validation

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -3,20 +3,22 @@
 Minimum Operations
 """
 
+
 def minOperations(n: int) -> int:
-    """Calculates the fewest number of operations needed to achieve n 'H' characters."""
+    """Calculates the fewest number of
+    operations needed to achieve n 'H' characters."""
     if n <= 1:
         return 0
-    
+
     operations = 0
     divisor = 2
 
     # Factorizing n by dividing by the smallest factors
     while n > 1:
-        # If the current divisor is a factor, reduce n and add the divisor to the operations count
+        # If the current divisor is a factor, reduce n and add the divisor
         while n % divisor == 0:
             operations += divisor
             n //= divisor
         divisor += 1
-    
+
     return operations
